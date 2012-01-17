1ca96d563da61ff7ba1ce063d2cc09f10f3d87fe
Sibi <psibi2000@gmail.com>
2012-01-17 19:41:30 +0530

    Initial Commit

---
+++ ajaxHandler.php
@@ -0,0 +1,88 @@
+<?php /*class section division is for splitting into various divisions. */
+$more = '
+    <div class="section"> 
+      <h2>Work Experience</h2>
+      <div class="item">
+        <h3><a href="http://drdo.gov.in">Defence Research and Development Organisation</a></h3>
+        <h4>Embedded Programmer</h4>
+        <div class="date">March-June 2011</div>
+        <div class="description">
+          <p align="justify">
+            Worked on the Real Time Embedded Computers lab of Research Centre Imarat of DRDO for a project. Developed a graphical user interface application for Serial Communication between a host and an embedded system. The developed software has the following features:
+<ul><li>A Serial Port Driver for communicating with the Serial Port.</li>
+<li>Make and Transmit packets to the OBP (On-Board Processor) according to their specified protocol.</li>
+<li>Receive packets from the OBP.</li>
+<li>Performing computations like checksum for determining the integrity of the sent and the received packet.</li>
+<li>Logging real time datas like dynamic pressure, time, lift off value etc to a log file.</li>
+</ul>
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <h3>S-FOSS</h3>
+        <h4>Linux Tutor</h4>
+        <div class="date">2009 - 2010</div>
+        <div class="description">
+          <p align="justify">I tutored students in open source technologies and how to use Linux platform for their daily use. All the classes were conducted in SASTRA University. Most of them were undergraduate students. I worked with the students for making them familiar with the history of open source and how to work comfortably on a KDE and a GNOME Desktop environment.
+</p><p>
+Students were given a basic understanding of the file system hierarchy found in basic UNIX systems. Basic shell scripting was taught to them.</p>
+        </div>
+      </div>
+    </div>
+  <div class="section">
+    <h2>Education</h2>
+    <div class="item">
+        <h3><a href="http://sastra.edu">SASTRA University, IN</a></h3>
+        <h4>B.Tech, Computer Science and Engineering</h4>
+        <div class="date">2008 - 2012</div>
+        <div class="description">
+          <p align="justify"> Studied engineering in SASTRA. Got diverted towards Machine Learning and Artifical Intelligence. Presented a paper related to Climate Prediction model using 
+Machine Learning algorithm at INEMREC seminar organized by Indian Meterological Society.
+<ul> <li> Ambassador of <strong> openSUSE </strong> project</li>
+<li> Dean\'s List of Merit Certificate </li>
+</ul></p>
+        </div>
+      </div>
+      <div class="item">
+        <h3>Nirmala Convent Inter College</h3>
+        <h4>General Schooling/High School</h4>
+        <div class="date">2003 - 2008</div>
+        <div class="description">
+          <p>Topped the ISC examination.</p>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <h2>Skills</h2>
+      <div class="item">
+        <h3>Programming Languages</h3>
+        <div class="description">
+          <p>C, C++, Java, VB .NET, C# .NET, VC++ .NET, Python</p>
+        </div>
+      </div>
+      <div class="item">
+        <h3>Scripting Languages</h3>
+        <div class="description">
+          <p>BASH</p>
+        </div>
+      </div>
+      <div class="item">
+        <h3>Web Technologies</h3>
+        <div class="description">
+          <p>HTML, CSS, Javascript, jQuery</p>
+        </div>
+      </div>
+      <div class="item">
+        <h3>DBMS</h3>
+        <div class="description">
+          <p>MySQL, Oracle</p>
+        </div>
+      </div>
+    </div>
+
+        ';              
+  $type = $_REQUEST['type'];
+  if ($type == "MORE"){
+    echo $more; 
+  }
+?>
---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE>
+<html>
+<head>
+  <title>Sibi</title>
+  
+  <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />   
+  <link rel="stylesheet" href="js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+  <script type="text/javascript" src="js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <script type="text/javascript" src="/fancybox/jquery.easing-1.4.pack.js"></script>
+  <script src="js/functions.js" type="text/javascript"></script>
+  <script>
+    function fetchReq(choice){
+      if (choice="MORE") {
+        $('#more').html('<img class="loader" src =images/ajax-loader.gif />'); //It will select element with ID=more. and html() will change the contents as provide inside it.
+        $.get('ajaxHandler.php',{'type':choice},function(data) {
+          $('#more').fadeIn('slow'); //Gradually changes visibility from hidden to visibile.
+          $('#more').html(data);
+        });
+        $("#More").css("visibility", "hidden");//Hiding the See More button 
+        $("#tabs").show(); //Now making the id="tabs" Visible
+      }
+    }
+</script>
+<script type="text/javascript">
+		$(document).ready(function() {
+$("a#pic").fancybox({
+				'transitionIn'	: 'elastic',
+				'transitionOut'	: 'elastic'
+			});
+});
+	</script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27311575-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+ 
+</head>
+<link rel="shortcut icon" href="favicon.ico"> <!-- For displaying icon in address bar -->
+<body>
+  <div id="header"> 
+    <ul>
+      <div id="tabs" style="display: none"> <!-- Dont display them now. -->
+        <li><a href="http://blog.psibi.in" target="_blank"><span>Blog</span></a></li>
+	<li><a href="http://psibi.users.sourceforge.net" target="_blank"><span> SourceForge </span> </li>
+        </div>
+      <li><a href="#" id="More" onclick=fetchReq("MORE");><span>See More</span></a></li> <!--On clicking it the function fetchReq will be called. href="#" is used to specify that anchor is used for JS code. -->
+    </ul>
+  </div>
+  <div id="about">
+    <div id="story">
+      <h1>Sibi</h1>
+      <h2>-- Programmer / Hacker / Free Software Enthusiast</h2>
+      <p>Any <strong>intelligent fool</strong> can make things bigger and more complex... It takes a touch of genius - and a lot of courage to move in the opposite direction.</p>
+      
+    </div>
+    <ul id="contact">
+      <li><span>Phone</span> <strong><span class="phone">+91 7200-120-343</span></strong></li>
+      <li><span>Handle</span> <strong><a href="http://psibi.in">psibi</a></strong></li>
+      
+      <li><span>Resume</span> <strong><a href="resume.pdf">PDF</a> </strong></li>
+      <li><span>Email</span> <strong>sibi@psibi.in</strong></li>
+      <li>
+        <div class="social">
+          <a href="http://facebook.com/psibi" target="_blank"><img src="images/social/facebook.png" alt="Friend me on Facebook" /></a> <!-- target="-blank" opens the new window in a tab -->
+	   <a href="https://plus.google.com/u/0/102869108986715355195/about" target="_blank"><img src="images/social/gplus.png" alt="Circle me on Google+" /></a>
+          <a href="http://twitter.com/psibi" target="_blank"><img src="images/social/twitter.png" alt="Follow me in twitter" /></a> 
+          <a href="http://linkedin.com/in/psibi" target="_blank"><img src="images/social/linkedin.png" alt="Connect in Linkedin" /></a> 
+          <a href="http://identi.ca/psibi" target="_blank"><img src="images/social/identica.png" alt="Follow me in identi.ca" /></a> 
+          <a href="https://github.com/psibi" target="_blank"><img src="images/social/github.png" alt="Check out my github profile" /></a>
+        </div>
+      </li>
+    </ul>
+<a id="pic" href="images/sibi.jpg" title="Sibi"><img id="picture" alt="Sibi" src="images/sibi.jpg" /></a>
+      </div>
+  <div id="more">  <!--This division is for displaying data when the button See More is clicked-->
+  </div>
+  <div id="footer">
+    <p>&copy; Sibi | psibi.in | <small>Last updated on Nov. of '11.</small></p>
+  </div>    
+</body>
+</html>
